14b37f1fe27fa8865dbdff3b87e4ff0a2e6cf64f
liuzuming <1609242720@qq.com>
2024-09-02 17:23:22 +0800

    这是全局统一返回和全局拦截，部分没开发完2024，09，02

--- com.lzm.springboot/lab3-quanJuTongYiFangHui/src/main/java/org/lzm/config/SpringMVCConfiguration.java
+++ com.lzm.springboot/lab3-quanJuTongYiFangHui/src/main/java/org/lzm/config/SpringMVCConfiguration.java
@@ -4,15 +4,28 @@ package org.lzm.config;
 import org.lzm.common.interceptor.FirstInterceptor;
 import org.lzm.common.interceptor.SecondInterceptor;
 import org.lzm.common.interceptor.ThirdInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
 
 @Configuration
 public class SpringMVCConfiguration implements WebMvcConfigurer {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Bean
     public FirstInterceptor firstInterceptor() {
@@ -38,5 +51,51 @@ public class SpringMVCConfiguration implements WebMvcConfigurer {
         // 拦截器三
         registry.addInterceptor(this.thirdInterceptor()).addPathPatterns("/**");
     }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(new HttpServlet() {
+
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                //super.doGet(req, resp);
+                logger.info("doGet..[uri:{}]", req.getRequestURI());
+            }
+        });
+        servlet.setUrlMappings(Collections.singleton("test/01"));
+        return servlet;
+    }
+
+    @Bean
+    public FilterRegistrationBean testFilter01() {
+        FilterRegistrationBean filter = new FilterRegistrationBean(new Filter() {
+
+            @Override
+            public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+                logger.info("[doFilter]");
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        });
+        filter.setUrlPatterns(Collections.singletonList("/test/*"));
+        return filter;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<?> testListener01() {
+        ServletListenerRegistrationBean<ServletContextListener> bean = new ServletListenerRegistrationBean<>(new ServletContextListener() {
+            @Override
+            public void contextInitialized(ServletContextEvent sce) {
+                //ServletContextListener.super.contextInitialized(sce);
+                logger.info("[contextInitialized]");
+            }
+
+            @Override
+            public void contextDestroyed(ServletContextEvent sce) {
+                ServletContextListener.super.contextDestroyed(sce);
+            }
+        });
+        return bean;
+    }
+
 
 }
